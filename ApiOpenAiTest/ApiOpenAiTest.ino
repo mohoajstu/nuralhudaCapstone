e66c6112eae92a57961e7662ae06d981ef9d6dfa
QGH <76548491+QGH11@users.noreply.github.com>
2025-01-24 13:24:30 -0800

    ..

--- ApiOpenAiTest/ApiOpenAiTest.ino
+++ ApiOpenAiTest/ApiOpenAiTest.ino
@@ -11,7 +11,7 @@ const char* password = "password";
 
 // API endpoint
 const char* server = "api.openai.com";
-const char* apiKey = "key";
+// const char* apiKey = "key";
 
 const char* openai_cert = R"EOF(
 -----BEGIN CERTIFICATE-----
